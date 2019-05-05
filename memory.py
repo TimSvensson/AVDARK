1bb6b0c09d40712a6da36053aa267c9c7d84e8bf
Tim Svensson <svensson_tim@hotmail.se>
2019-05-05 22:55:23 +0200

    changes in memory.py

--- memory.py
+++ memory.py
@@ -4,12 +4,20 @@ class memory:
         self.size = size
         self.data = [0] * self.size
 
-        #def __str__(self):
+        #def __repr__(self):
         #    pass
 
-    def __repr__(self):
+    def __str__(self):
+        s = "<{}, self.size: {}\n", self.__class__.__name__, self.size)
+        lbound = 0
+        while lbound < self.size:
+            rbound = lbound + 10 < self.size ? lbound + 10 : self.size
+            s += "".format()
+
+        return s
         return "<{}, self.size: {}, self.data: {}>".format(
             self.__class__.__name__, self.size, self.data)
+    
 
     def store(self, data, address):
         if address < 0 or address > self.size:
